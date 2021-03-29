c96078a5820878eab003aa3f6803a9dbe3e5e68a
Tamir Kamara <26870601+tamirkamara@users.noreply.github.com>
2021-03-29 11:30:42 +0300

    use a terraform RG name (#104)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,9 @@ terraform {
   }
 
   backend "azurerm" {
+    # including the resource group allows us to supply just 1 secret for access.
+    # however, this assumes that the terraform state storage account is on the same subscription as the resources deployed.
+    resource_group_name  = "rg-terraform-backend"
     storage_account_name = "oxterraformbackend"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
