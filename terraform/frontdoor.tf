163489f7a41a42c990dff8d02480602a09d3c6cb
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-05-07 15:42:36 +0100

    arts933

--- terraform/frontdoor.tf
+++ terraform/frontdoor.tf
@@ -84,6 +84,5 @@ resource "azurerm_frontdoor" "frontdoor" {
   frontend_endpoint {
     name      = local.frontend_endpoint
     host_name = "fd-${var.trial_name}-${var.environment}.azurefd.net"
-    custom_https_provisioning_enabled = false
   }
 }
