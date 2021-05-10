d9cdb032da85b9d859bca43c5f2aa4ad79d73e08
Darren Smithson <darrengsmithson@hotmail.com>
2021-05-10 08:46:49 +0100

    Update services.tf

--- terraform/services.tf
+++ terraform/services.tf
@@ -11,8 +11,8 @@ locals {
     "SPRING_CLOUD_CONFIG_LABEL"             = var.spring_config_label
     "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.app_insights.connection_string
     "EUREKA_CLIENT_SERVICEURL_DEFAULTZONE"  = "${module.trial_sc_discovery.hostname}/eureka/"
-    "WEBSITE_DNS_SERVER"     = "168.63.129.16"
-    "WEBSITE_VNET_ROUTE_ALL" = 1
+    "WEBSITE_DNS_SERVER"                    = "168.63.129.16"
+    "WEBSITE_VNET_ROUTE_ALL"                = 1
   }
 }
 
