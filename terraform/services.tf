ef6413fb98af36ffc7dc013135a7d98283a25aed
LizaShak <lishakur@microsoft.com>
2021-06-13 17:40:26 +0300

    priorities test

--- terraform/services.tf
+++ terraform/services.tf
@@ -136,7 +136,7 @@ module "roles_sql_server" {
   location               = azurerm_resource_group.trial_rg.location
   rg_name                = azurerm_resource_group.trial_rg.name
   primary_location       = var.location
-  failover_name          = "failover-roles-dr3"
+  failover_name          = "failover-roles-${var.trial_name}"
   db_name                = "ROLES"
   app_name               = "roles"
   sql_user               = "rolesuser"
--- terraform/services_fhir.tf
+++ terraform/services_fhir.tf
@@ -50,7 +50,7 @@ module "fhir_sql" {
   rg_name                 = azurerm_resource_group.trial_rg.name
   location                = azurerm_resource_group.trial_rg.location
   primary_location        = var.location
-  failover_name           = "failover-fhir-dr3"
+  failover_name           = "failover-fhir-${var.trial_name}"
   db_name                 = "FHIR"
   app_name                = "fhir"
   sql_user                = "fhiruser"
