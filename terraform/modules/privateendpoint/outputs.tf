90e8a958da367cb36a8070260ec39073738f299d
LizaShak <lishakur@microsoft.com>
2021-06-16 18:41:41 +0300

    fix pe

--- terraform/modules/privateendpoint/outputs.tf
+++ terraform/modules/privateendpoint/outputs.tf
@@ -1,4 +1,4 @@
 output "private_endpoint_ip" {
-  value       = azurerm_private_endpoint.private_endpoint.private_service_connection.private_ip_address
+  value       = azurerm_private_endpoint.private_endpoint.private_service_connection[0].private_ip_address
   description = "The private endpoint private ip address."
 }
