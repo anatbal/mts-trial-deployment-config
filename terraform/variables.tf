0d9638899e87ba2f7784d38475e76752796a86da
Anat Balzam <anatbaz@gmail.com>
2021-10-27 11:42:25 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,7 @@ variable "environment" {
 variable "location" {
   type        = string
   description = "Azure region where to create resources."
-  default     = "westus"
+  default     = "centralus"
 }
 
 variable "failover_location" {
